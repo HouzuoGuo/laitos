0c7d94f31bfe0563d0614eda56725c2b6a535158
HouzuoGuo <guohouzuo@gmail.com>
2017-02-16 21:20:39 +0100

    i cannot trust terminal output anymore

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -1,11 +1,129 @@
 package bridge
 
-import "github.com/HouzuoGuo/websh/feature"
+import (
+	"bytes"
+	"fmt"
+	"github.com/HouzuoGuo/websh/feature"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+	"unicode"
+)
 
+var RegexConsecutiveSpaces = regexp.MustCompile("[[:space:]]+")
+
+// Provide transformation feature for input command.
 type CommandBridge interface {
 	Transform(feature.Command) feature.Command
 }
 
+// Provide transformation feature for command result.
 type ResultBridge interface {
 	Transform(string) string
+}
+
+// Expand shortcuts to full commands.
+type CommandShortcuts struct {
+	Shortcuts map[string]string
+}
+
+func (short *CommandShortcuts) Transform(cmd feature.Command) feature.Command {
+	if short.Shortcuts == nil {
+		return cmd
+	}
+	if shortcut, exists := short.Shortcuts[strings.TrimSpace(cmd.Content)]; exists {
+		ret := cmd
+		ret.Content = shortcut
+		return ret
+	}
+	return cmd
+}
+
+// Translate character sequences to something different.
+type CommandTranslator struct {
+	Sequences [][]string
+}
+
+func (tr *CommandTranslator) Transform(cmd feature.Command) feature.Command {
+	if tr.Sequences == nil {
+		return cmd
+	}
+	newContent := cmd.Content
+	for _, tuple := range tr.Sequences {
+		if len(tuple) != 2 {
+			continue
+		}
+		newContent = strings.Replace(newContent, tuple[0], tuple[1], -1)
+	}
+	ret := cmd
+	ret.Content = newContent
+	return ret
+}
+
+/*
+Lint string in the following order (each step is turned on by respective attribute)
+1. Trim all leading and trailing spaces from lines.
+2. Compress all lines into a single line, joint by a semicolon.
+3. Retain only printable & visible, 7-bit ASCII characters.
+4. Compress consecutive spaces into single space - this will also cause all lines to squeeze.
+5. Remove a number of leading character.
+6. Remove excessive characters at end of the string.
+*/
+type StringLint struct {
+	TrimSpaces              bool
+	CompressToSingleLine    bool
+	KeepVisible7BitCharOnly bool
+	CompressSpaces          bool
+	BeginPosition           int
+	MaxLength               int
+}
+
+func (lint *StringLint) Transform(in string) string {
+	ret := in
+	// Trim
+	if lint.TrimSpaces {
+		var out bytes.Buffer
+		for _, line := range strings.Split(in, "\n") {
+			out.WriteString(strings.TrimSpace(line))
+			out.WriteRune('\n')
+		}
+		ret = out.String()
+	}
+	// Compress lines
+	if lint.CompressToSingleLine {
+		ret = strings.Replace(in, "\n", ";", -1)
+		time.Sleep(2 * time.Second)
+		panic(ret)
+		fmt.Fprintf(os.Stderr, "RET2 is =========\n%v\n========\n", []byte(ret))
+	}
+	// Retain printable chars
+	if lint.KeepVisible7BitCharOnly {
+		var out bytes.Buffer
+		for _, r := range ret {
+			if r < 128 && unicode.IsPrint(r) {
+				out.WriteRune(r)
+			}
+		}
+		ret = out.String()
+	}
+	// Compress consecutive spaces
+	if lint.CompressSpaces {
+		ret = RegexConsecutiveSpaces.ReplaceAllString(ret, " ")
+	}
+	// Cut leading characters
+	if lint.BeginPosition > 0 {
+		if len(ret) > lint.BeginPosition {
+			ret = ret[lint.BeginPosition:]
+		} else {
+			ret = ""
+		}
+	}
+	// Cut trailing characters
+	if lint.MaxLength > 0 {
+		if len(ret) > lint.MaxLength {
+			ret = ret[0:lint.MaxLength]
+		}
+	}
+	return ret
 }
--- bridge/bridge_test.go
+++ bridge/bridge_test.go
@@ -1,1 +1,63 @@
 package bridge
+
+import (
+	"github.com/HouzuoGuo/websh/feature"
+	"testing"
+)
+
+func TestCommandShortcuts_Transform(t *testing.T) {
+	short := CommandShortcuts{}
+	if out := short.Transform(feature.Command{Content: "abc"}); out.Content != "abc" {
+		t.Fatal(out)
+	}
+	short.Shortcuts = map[string]string{"abc": "123", "def": "456"}
+	if out := short.Transform(feature.Command{Content: ""}); out.Content != "" {
+		t.Fatal(out)
+	}
+	if out := short.Transform(feature.Command{Content: "abc"}); out.Content != "123" {
+		t.Fatal(out)
+	}
+	if out := short.Transform(feature.Command{Content: " def "}); out.Content != "456" {
+		t.Fatal(out)
+	}
+	if out := short.Transform(feature.Command{Content: "ghi"}); out.Content != "ghi" {
+		t.Fatal(out)
+	}
+}
+
+func TestCommandTranslator_Transform(t *testing.T) {
+	tr := CommandTranslator{}
+	if out := tr.Transform(feature.Command{Content: "abc"}); out.Content != "abc" {
+		t.Fatal(out)
+	}
+	tr.Sequences = [][]string{{"abc", "123"}, {"def", "456"}}
+	if out := tr.Transform(feature.Command{Content: ""}); out.Content != "" {
+		t.Fatal(out)
+	}
+	if out := tr.Transform(feature.Command{Content: " abc def "}); out.Content != " 123 456 " {
+		t.Fatal(out)
+	}
+	if out := tr.Transform(feature.Command{Content: " ghi "}); out.Content != " ghi " {
+		t.Fatal(out)
+	}
+}
+
+func TestStringLint_Transform(t *testing.T) {
+	lint := StringLint{}
+	if out := lint.Transform(""); out != "" {
+		t.Fatal(out)
+	}
+	mixedString := "abc  def 123 \r\t\n @#$<>\r\t\n 任意的"
+	if out := lint.Transform(mixedString); out != mixedString {
+		t.Fatal(out)
+	}
+	lint.TrimSpaces = true
+	lint.CompressToSingleLine = true
+	lint.KeepVisible7BitCharOnly = true
+	lint.CompressSpaces = true
+	lint.BeginPosition = 2
+	lint.MaxLength = 14
+	if out := lint.Transform(mixedString); out != "c def 123 ;@#" {
+		t.Fatal(out)
+	}
+}
