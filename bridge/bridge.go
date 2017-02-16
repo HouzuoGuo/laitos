cc7376dbc95c91cb73d935608218ab62d80040e7
HouzuoGuo <guohouzuo@gmail.com>
2017-02-16 16:55:41 +0100

    prepare bridges

--- bridge/bridge.go
+++ bridge/bridge.go
@@ -1,1 +1,11 @@
 package bridge
+
+import "github.com/HouzuoGuo/websh/feature"
+
+type CommandBridge interface {
+	Transform(feature.Command) feature.Command
+}
+
+type ResultBridge interface {
+	Transform(string) string
+}
