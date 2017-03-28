95f4bc2b562d71013707fcf97d68934827440fa4
HouzuoGuo <guohouzuo@gmail.com>
2017-03-28 17:01:58 +0200

    progress with imap fetching mail body

--- feature/imaps.go
+++ feature/imaps.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"log"
 	"math/rand"
@@ -33,8 +34,8 @@ func randomChallenge() string {
 	return strconv.Itoa(1000000000 + rand.Intn(1000000000))
 }
 
-// Send an IMAP command and wait for a response, then return.
-func (mbox *IMAPS) Converse(action string) (ok bool, status, body string, err error) {
+// Send an IMAP command and wait for a response, then return. If response status is not OK, an error is returned.
+func (mbox *IMAPS) Converse(action string) (status, body string, err error) {
 	var allLines bytes.Buffer
 	reader := bufio.NewReader(mbox.tlsConn)
 	challenge := randomChallenge()
@@ -43,50 +44,47 @@ func (mbox *IMAPS) Converse(action string) (ok bool, status, body string, err er
 	_, err = mbox.tlsConn.Write([]byte(fmt.Sprintf("%s %s\r\n", challenge, action)))
 	if err != nil {
 		// TLS internal state is surely corrupted after a timeout of write operation
-		goto badConversation
+		goto badIO
 	}
 	for {
 		var line []byte
 		line, _, err = reader.ReadLine()
-		fmt.Println("Read line ", string(line))
 		if err != nil {
 			/*
 				Even though TLS internal state is not corrupted after a timeout of read operation,
 				the IMAP conversation sequence is surely broken, hence close the connection.
 			*/
-			goto badConversation
+			goto badIO
 		}
 		lowerLine := strings.TrimSpace(strings.ToLower(string(line)))
 		if strings.Index(lowerLine, challenge) == 0 {
 			// Conversation is finished
 			body = allLines.String()
-			fmt.Println("Body is", body)
 			withoutChallenge := strings.TrimSpace(lowerLine[len(challenge):])
-			fmt.Println("Without challenge", withoutChallenge)
 			// Status word is at the beginning
 			afterStatusWord := strings.IndexRune(withoutChallenge, ' ')
 			if afterStatusWord == -1 {
 				status = withoutChallenge
 				err = fmt.Errorf("Cannot find status word among line - %s", withoutChallenge)
-				goto badConversation
+				goto badIO
 			}
 			statusWord := withoutChallenge[:afterStatusWord]
-			fmt.Println("Status word", statusWord)
-			if statusWord == "ok" {
-				ok = true
-			}
 			if len(withoutChallenge) > afterStatusWord {
 				status = withoutChallenge[afterStatusWord:]
+			}
+			if statusWord != "ok" {
+				err = fmt.Errorf("Bad status - %s", status)
 			}
 			return
 		} else {
 			// Continue to receive response body
 			allLines.Write(line)
+			allLines.WriteRune('\n')
 		}
 	}
 	return
-badConversation:
-// Close connection so that no further conversation may take place
+badIO:
+	// Close connection so that no further conversation may take place
 	mbox.tlsConn.Close()
 	mbox.conn.Close()
 	return
@@ -94,11 +92,9 @@ badConversation:
 
 // Get number of messages in the mail box.
 func (mbox *IMAPS) GetNumberMessages() (int, error) {
-	ok, status, body, err := mbox.Converse(fmt.Sprintf("STATUS %s (MESSAGES)", mbox.MailboxName))
+	_, body, err := mbox.Converse(fmt.Sprintf("STATUS %s (MESSAGES)", mbox.MailboxName))
 	if err != nil {
 		return 0, err
-	} else if !ok {
-		return 0, fmt.Errorf("STATUS command failed - %s", status)
 	}
 	// Extract number of messages from response body
 	numberString := regexp.MustCompile(`\d+`).FindString(body)
@@ -112,8 +108,88 @@ func (mbox *IMAPS) GetNumberMessages() (int, error) {
 	return number, nil
 }
 
+// Retrieve mail header from specified message number range.
+func (mbox *IMAPS) GetHeaders(from, to int) (ret map[int]string, err error) {
+	ret = make(map[int]string)
+	if from > to || from < 1 || to < 1 {
+		err = errors.New("From number must be less or equal to To number, and both must be positive.")
+		return
+	}
+	_, body, err := mbox.Converse(fmt.Sprintf("FETCH %d:%d BODY.PEEK[HEADER]", from, to))
+	if err != nil {
+		return
+	}
+	// Walk through body line by line to find boundary of messages
+	var thisNumber int
+	var thisMessage bytes.Buffer
+	for _, line := range strings.Split(body, "\n") {
+		trimmedLine := strings.TrimSpace(line)
+		if len(trimmedLine) == 0 {
+			continue
+		} else if len(trimmedLine) > 0 && trimmedLine[0] == '*' {
+			// Marks beginning of a message
+			// Store existing message
+			if thisMessage.Len() > 0 {
+				// Only store valid message
+				ret[thisNumber] = thisMessage.String()
+				thisNumber = 0
+				thisMessage.Reset()
+			}
+			// Parse current message number
+			thisNumberStr := regexp.MustCompile(`\d+`).FindString(trimmedLine)
+			thisNumber, _ = strconv.Atoi(thisNumberStr)
+		} else if trimmedLine == ")" {
+			// ) on its own line signifies end of message
+			if thisMessage.Len() > 0 {
+				ret[thisNumber] = thisMessage.String()
+			}
+		} else {
+			// Place the line in the current message buffer
+			thisMessage.WriteString(line)
+			thisMessage.WriteRune('\n')
+		}
+	}
+	return
+}
+
+// Retrieve an entire mail message including header and body.
+func (mbox *IMAPS) GetMessage(num int) (message string, err error) {
+	if num < 1 {
+		err = errors.New("Message number must be positive")
+		return
+	}
+	// Fetch the entire header section
+	headerSection, err := mbox.GetHeaders(num, num)
+	if err != nil {
+		return
+	}
+	entireHeader, found := headerSection[num]
+	if !found {
+		err = fmt.Errorf("Failed to retrieve message %d header", num)
+		return
+	}
+	// There is a "\r\n" in between header and body in a full message
+	var entireMessage bytes.Buffer
+	entireMessage.WriteString(entireHeader)
+	entireMessage.WriteString("\r\n")
+	_, body, err := mbox.Converse(fmt.Sprintf("FETCH %d BODY[TEXT]", num))
+	for _, line := range strings.Split(body, "\n") {
+		if len(line) > 0 {
+			switch line[0] {
+			// Skip fetch boundary lines
+			case '*', ')':
+				continue
+			}
+		}
+		entireMessage.WriteString(line)
+		entireMessage.WriteRune('\n')
+	}
+	message = entireMessage.String()
+	return
+}
+
 // Set up TLS connection to IMAPS server and log the user in.
-func (mbox *IMAPS) ConnectLogin() (err error) {
+func (mbox *IMAPS) ConnectLoginSelect() (err error) {
 	mbox.conn, err = net.DialTimeout(
 		"tcp",
 		fmt.Sprintf("%s:%d", mbox.Host, mbox.Port),
@@ -134,88 +210,24 @@ func (mbox *IMAPS) ConnectLogin() (err error) {
 	if err != nil {
 		return fmt.Errorf("Failed to read server greeting - %v", err)
 	}
-	ok, status, _, err := mbox.Converse(fmt.Sprintf("LOGIN %s %s", mbox.AuthUsername, mbox.AuthPassword))
+	// LOGIN && SELECT
+	_, _, err = mbox.Converse(fmt.Sprintf("LOGIN %s %s", mbox.AuthUsername, mbox.AuthPassword))
 	if err != nil {
-		return fmt.Errorf("Failed to send login request - %v", err)
-	} else if !ok {
-		return fmt.Errorf("LOGIN command failed - %s", status)
+		return fmt.Errorf("LOGIN command failed - %v", err)
+	}
+	_, _, err = mbox.Converse(fmt.Sprintf("SELECT %s", mbox.MailboxName))
+	if err != nil {
+		return fmt.Errorf("SELECT command failed - %v", err)
 	}
 	return
 }
 
-func (mbox *IMAPS) DisconnectLogout() error {
-	mbox.Converse("LOGOUT")
-}
-
-func main() {
-	mbox := IMAPS{
-		Host:               "A",
-		Port:               993,
-		InsecureSkipVerify: true,
-		AuthUsername:       "B",
-		AuthPassword:       "C",
-		IOTimeoutSec:       10,
+func (mbox *IMAPS) DisconnectLogout() {
+	if _, _, err := mbox.Converse("LOGOUT"); err != nil {
+		log.Printf("IMAPS.DisconnectLogout: LOGOUT command failed - %v", err)
 	}
-	if err := mbox.ConnectLogin(); err != nil {
-		log.Fatal(err)
-	}
-
-	conn, err := net.Dial("tcp", "A:993")
-	if err != nil {
-		log.Fatal(err)
-	}
-	tlsConn := tls.Client(conn, &tls.Config{InsecureSkipVerify: true})
-	if err := tlsConn.Handshake(); err != nil {
-		log.Fatal(err)
-	}
-	go func() {
-		reader := bufio.NewReader(tlsConn)
-		for {
-			line, isPrefix, err := reader.ReadLine()
-			if err != nil {
-				log.Fatal("Read", err)
-			}
-			fmt.Printf("%v %s\n", isPrefix, line)
-		}
-	}()
-	writer := bufio.NewWriter(tlsConn)
-	time.Sleep(1 * time.Second)
-	fmt.Println("LOGIN")
-	if _, err := writer.WriteString("9223372036854775808 LOGIN A B\r\n"); err != nil {
-		log.Fatal(err)
-	}
-	writer.Flush()
-	time.Sleep(1 * time.Second)
-	fmt.Println("SELECT")
-	if _, err := writer.WriteString("? SELECT INBOX\r\n"); err != nil {
-		log.Fatal(err)
-	}
-	writer.Flush()
-	time.Sleep(1 * time.Second)
-	fmt.Println("FETCH")
-	if _, err := writer.WriteString("? FETCH 1:2 BODY.PEEK[HEADER]\r\n"); err != nil {
-		log.Fatal(err)
-	}
-	writer.Flush()
-	time.Sleep(1 * time.Second)
-	fmt.Println("FETCH BODY")
-	if _, err := writer.WriteString("? FETCH 1:2 BODY[TEXT]\r\n"); err != nil {
-		log.Fatal(err)
-	}
-	time.Sleep(1 * time.Second)
-	fmt.Println("CLOSE")
-	if _, err := writer.WriteString("? CLOSE INBOX\r\n"); err != nil {
-		log.Fatal(err)
-	}
-	writer.Flush()
-	time.Sleep(1 * time.Second)
-	fmt.Println("LOGOUT")
-	if _, err := writer.WriteString("? LOGOUT\r\n"); err != nil {
-		log.Fatal(err)
-	}
-	writer.Flush()
-	fmt.Println("Finished and waiting")
-	time.Sleep(2 * time.Second)
+	mbox.tlsConn.Close()
+	mbox.conn.Close()
 }
 
 // Correspond IMAP account connection details to account names.
@@ -226,7 +238,15 @@ type IMAPAccounts struct {
 var TestIMAPAccounts = IMAPAccounts{} // Account details are set by init_feature_test.go
 
 func (imap *IMAPAccounts) IsConfigured() bool {
-	return imap.Accounts != nil && len(imap.Accounts) > 0
+	if imap.Accounts == nil || len(imap.Accounts) == 0 {
+		return false
+	}
+	for _, account := range imap.Accounts {
+		if account.Host == "" || account.Port == 0 || account.MailboxName == "" {
+			return false
+		}
+	}
+	return true
 }
 
 func (imap *IMAPAccounts) SelfTest() error {
@@ -234,7 +254,7 @@ func (imap *IMAPAccounts) SelfTest() error {
 		return ErrIncompleteConfig
 	}
 	for name, account := range imap.Accounts {
-		if err := account.ConnectLogin(); err != nil {
+		if err := account.ConnectLoginSelect(); err != nil {
 			return fmt.Errorf("Account \"%s\" connection error - %v", name, err)
 		}
 		defer account.DisconnectLogout()
@@ -247,7 +267,7 @@ func (imap *IMAPAccounts) SelfTest() error {
 
 func (imap *IMAPAccounts) Initialise() error {
 	for name, account := range imap.Accounts {
-		if err := account.ConnectLogin(); err != nil {
+		if err := account.ConnectLoginSelect(); err != nil {
 			return fmt.Errorf("Account \"%s\" connection error - %v", name, err)
 		}
 	}
--- feature/imaps_test.go
+++ feature/imaps_test.go
@@ -1,6 +1,9 @@
 package feature
 
-import "testing"
+import (
+	"github.com/HouzuoGuo/laitos/email"
+	"testing"
+)
 
 func TestIMAPS(t *testing.T) {
 	if !TestIMAPAccounts.IsConfigured() {
@@ -12,4 +15,45 @@ func TestIMAPS(t *testing.T) {
 	if err := TestIMAPAccounts.SelfTest(); err != nil {
 		t.Fatal(err)
 	}
+	// IMAPS account test
+	accountA := TestIMAPAccounts.Accounts["a"]
+	if err := accountA.ConnectLoginSelect(); err != nil {
+		t.Fatal(err)
+	}
+	if num, err := accountA.GetNumberMessages(); err != nil || num == 0 {
+		t.Fatal(num, err)
+	}
+	if _, err := accountA.GetHeaders(1, 0); err == nil {
+		t.Fatal("did not error")
+	}
+	if _, err := accountA.GetHeaders(2, 1); err == nil {
+		t.Fatal("did not error")
+	}
+	// Retrieve headers, make sure it is retrieving three different emails
+	headers, err := accountA.GetHeaders(1, 3)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(headers) != 3 {
+		t.Fatal(headers)
+	}
+	// Retrieve mail body
+	msg, err := accountA.GetMessage(1)
+	if err != nil {
+		t.Fatal(err, msg)
+	}
+	err = email.WalkMessage([]byte(msg), func(prop email.BasicProperties, section []byte) (bool, error) {
+		if prop.Subject == "" || prop.ContentType == "" || prop.FromAddress == "" || prop.ReplyAddress == "" {
+			t.Fatalf("%+v", prop)
+		}
+		return true, nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	accountA.DisconnectLogout()
+}
+
+func TestIMAPAccounts_Execute(t *testing.T) {
+
 }
