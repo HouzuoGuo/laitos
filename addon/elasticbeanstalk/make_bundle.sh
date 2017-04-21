b462abc02fe569f2d08b21a4616215d67f4155d7
HouzuoGuo <guohouzuo@gmail.com>
2017-04-21 09:34:39 +0200

    improve make bundle with phantomjs executable and permission settings

--- addon/elasticbeanstalk/make_bundle.sh
+++ addon/elasticbeanstalk/make_bundle.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
+[ "$(basename $(pwd))" != elasticbeanstalk ] && echo 'Please run the script from "elasticbeanstalk" directory.' && exit 1
+
 config_dir=$1
 [ -z "$config_dir" ] && echo "Usage: $0 configuration_dir" && exit 1
 [ ! -d "$config_dir" ] && echo "Configuration directory $config_dir does not exist" && exit 1
 
 set -e
 
-pushd ../
+pushd ../../
 go build
 popd
 
 rm -rf bundle/
 cp -R "$config_dir" bundle/
-cp -R ../laitos ./Procfile ./.ebextensions/ bundle/
+cp -R ../../laitos ./Procfile ./.ebextensions/ ../phantom* bundle/
+chmod -R 755 bundle/
 
 pushd bundle
 zip -r bundle.zip ./
