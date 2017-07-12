ad1003d7b7dd76c97e351cec6c38f9fe1c723fe4
HouzuoGuo <guohouzuo@gmail.com>
2017-07-12 20:07:47 +0200

    walk around phantomjs dom like a boss

--- addon/laitos.js
+++ addon/laitos.js
@@ -1,15 +1,18 @@
 try {
-    var browser; // browser is the browser page instance
+    var browser; // the browser page instance after very first URL is visited
 
     // ============== ACTIONS COMMON TO INTERACTIVE AND LINE-ORIENTED INTERFACE ==========
+
+    // Re-render page screenshot.
     var b_redraw = function () {
         if (!browser) {
             return false;
         }
-        browser.render('%s');
+        browser.render('render.jpg');
         return true;
     };
 
+    // Navigate back.
     var b_back = function () {
         if (!browser) {
             return false;
@@ -18,6 +21,7 @@ try {
         return true;
     };
 
+    // Navigate forward.
     var b_forward = function () {
         if (!browser) {
             return false;
@@ -26,6 +30,7 @@ try {
         return true;
     };
 
+    // Reload the current URL (refresh).
     var b_reload = function () {
         if (!browser) {
             return false;
@@ -34,6 +39,7 @@ try {
         return true;
     };
 
+    // Go to a new URL.
     var b_goto = function (param) {
         if (!browser) {
             browser = require('webpage').create();
@@ -56,6 +62,7 @@ try {
         return true;
     };
 
+    // Retrieve title and URL of the current page.
     var b_info = function () {
         var ret = '';
         if (browser) {
@@ -69,6 +76,7 @@ try {
         return ret;
     };
 
+    // Move mouse pointer to a coordinate and optionally click a button.
     var b_pointer = function (param) {
         if (!browser) {
             return false;
@@ -77,6 +85,7 @@ try {
         return true;
     };
 
+    // Type keys into the currently focused element.
     var b_type = function (param) {
         if (!browser) {
             return false;
@@ -89,6 +98,7 @@ try {
         return true;
     };
 
+    // Run a web server that receives commands from HTTP clients.
     var server = require('webserver').create().listen('127.0.0.1:12345', function (req, resp) {
         resp.statusCode = 200;
         resp.headers = {
@@ -97,6 +107,7 @@ try {
         };
         var ret = null;
         if (req.url === '/redraw') {
+            // curl -X POST 'localhost:12345/redraw'
             ret = b_redraw();
         } else if (req.url === '/back') {
             ret = b_back();
@@ -105,13 +116,27 @@ try {
         } else if (req.url === '/reload') {
             ret = b_reload();
         } else if (req.url === '/goto') {
+            // curl -X POST --data 'user_agent=user_agent=Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F59.0.3071.115%20Safari%2F537.36&view_width=1024&view_height=1024&page_url=https%3A%2F%2Fgoogle.com' 'localhost:12345/goto'
             ret = b_goto(req.post);
         } else if (req.url === '/info') {
+            // curl -X POST 'localhost:12345/info'
             ret = b_info();
         } else if (req.url === '/pointer') {
             ret = b_pointer(req.post);
         } else if (req.url === '/type') {
             ret = b_type(req.post);
+        } else if (req.url === '/lo_reset') {
+            // curl -X POST 'localhost:12345/lo_reset'
+            ret = b_lo_reset();
+        } else if (req.url === '/lo_next') {
+            // curl -X POST 'localhost:12345/lo_next'
+            ret = b_lo_next()
+        } else if (req.url === '/lo_prev') {
+            // curl -X POST 'localhost:12345/lo_prev'
+            ret = b_lo_prev()
+        } else if (req.url === '/lo_next_n') {
+            // curl -X POST --data 'n=10' 'localhost:12345/lo_next'
+            ret = b_lo_next_n(req.post);
         }
         console.log(req.method + ' ' + req.url + ' - ' + JSON.stringify(req.post) + ': ' + JSON.stringify(ret));
         resp.write(JSON.stringify(ret));
@@ -122,21 +147,27 @@ try {
     });
 
     // ===================== ONLY FOR LINE-ORIENTED INTERFACE =================
-    var last_info = null, prev_info = null;
 
+    // The very previous element and its own previous/next element that were navigated into.
+    var exact_info = null, before_info = null, after_info = null;
+
+    // Put a string into double quotes.
     var quote_str = function (str) {
         return JSON.stringify(str);
     };
 
+    // Return a string-encoded function body that store 4 element parameters into window object.
     var elem_info_to_stmt = function (elem_info) {
         return "function(){" +
             "window.laitos_pjs_tag = " + quote_str(elem_info === null ? '' : elem_info['tag']) + ";" +
             "window.laitos_pjs_id  = " + quote_str(elem_info === null ? '' : elem_info['id']) + ";" +
             "window.laitos_pjs_name = " + quote_str(elem_info === null ? '' : elem_info['name']) + ";" +
             "window.laitos_pjs_inner = " + quote_str(elem_info === null ? '' : elem_info['inner']) + ";" +
+            "window.laitos_pjs_stop_at_first = " + (elem_info === null ? 'true' : 'false') + ";" +
             "}";
     };
 
+    // Look for an element, and return brief details of the element along with its previous and next element.
     var eval_find_before_after = function () {
         var elem_to_obj = function (elem) {
             return {
@@ -163,12 +194,13 @@ try {
                 var height = elem.offsetHeight,
                     width = elem.offsetWidth,
                     elem_inner = elem.innerHTML;
+                // Only consider elements that are at least 9 square pixels large and content does not look exceedingly long
                 if (height > 3 && width > 3 && elem_inner.length < 1000) {
                     if (stop_next) {
                         after = elem;
                         return false;
                     }
-                    if (elem.tagName === tag && elem.id === id && elem.name === name && elem_inner === inner) {
+                    if (elem.tagName === tag && elem.id === id && elem.name === name && elem_inner === inner || laitos_pjs_stop_at_first) {
                         exact = elem;
                         stop_next = true;
                     } else {
@@ -183,43 +215,125 @@ try {
                 after === null ? null : elem_to_obj(after)
             ];
         };
-
+        // Invoke the search function using parameters stored in window object
         return find_before_after(laitos_pjs_tag, laitos_pjs_id, laitos_pjs_name, laitos_pjs_inner);
     };
 
+    // Reset previous element information, so that the next "next" action will find the first element.
     var b_lo_reset = function () {
-        prev_info = null;
-        last_info = null;
+        before_info = null;
+        exact_info = null;
     };
 
+    // Navigate to the next element.
     var b_lo_next = function () {
         if (!browser) {
             return false;
         }
+        if (exact_info === null) {
+            console.log('b_lo_next: visit the first element');
+            // Go to the first element, null parameter value will turn on laitos_pjs_stop_at_first.
+            browser.evaluateJavaScript(elem_info_to_stmt(exact_info));
+        } else {
+            if (after_info === null) {
+                // If already at last element, do not go further next.
+                console.log('b_lo_next: already at last element');
+                browser.evaluateJavaScript(elem_info_to_stmt(exact_info));
+            } else {
+                // Go to the next element
+                console.log('b_lo_next: visit the next element');
+                browser.evaluateJavaScript(elem_info_to_stmt(after_info));
 
-        browser.evaluateJavaScript(elem_info_to_stmt(last_info));
-        var ret = browser.evaluate(eval_find_before_after());
-
-        prev_info = ret[0];
-        last_info = ret[1];
+            }
+        }
+        var ret = browser.evaluate(eval_find_before_after);
+        before_info = ret[0];
+        exact_info = ret[1];
+        after_info = ret[2];
         return ret;
     };
 
+    // Navigate to the previous element.
     var b_lo_prev = function () {
         if (!browser) {
             return false;
         }
 
-        browser.evaluateJavaScript(elem_info_to_stmt(prev_info));
-        var ret = browser.evaluate(eval_find_before_after());
+        browser.evaluateJavaScript(elem_info_to_stmt(before_info));
+        var ret = browser.evaluate(eval_find_before_after);
 
-        prev_info = ret[0];
-        last_info = ret[1];
+        before_info = ret[0];
+        exact_info = ret[1];
+        after_info = ret[2];
         return ret;
     };
 
-    var b_lo_next_n = function (n) {
+    // Jump forward for a designated number of elements, and return information of elements seen on the way.
+    var b_lo_next_n = function (param) {
+        if (!browser) {
+            return false;
+        }
+        // If no element has ever been navigated into, go to the first element.
+        if (exact_info === null) {
+            b_lo_next();
+        }
+        browser.evaluateJavaScript(elem_info_to_stmt(exact_info));
+        browser.evaluateJavaScript("function(){window.laitos_pjs_next_n=" + param.n + ";}");
 
+        var ret = browser.evaluate(function () {
+            var elem_to_obj = function (elem) {
+                return {
+                    "tag": elem.tagName,
+                    "id": elem.id,
+                    "name": elem.name,
+                    "value": elem.value,
+                    "inner": elem.innerHTML
+                };
+            };
+
+            var walk = function (elem, walk_fun) {
+                for (var child = elem.childNodes, t = 0; t < child.length; t++) {
+                    if (!walk(child[t], walk_fun)) {
+                        return false;
+                    }
+                }
+                return walk_fun(elem);
+            };
+
+            var find_after = function (tag, id, name, inner, num) {
+                var ret = [], matched = false;
+                walk(document.documentElement, function (elem) {
+                    var height = elem.offsetHeight,
+                        width = elem.offsetWidth,
+                        elem_inner = elem.innerHTML;
+                    // Only consider elements that are at least 9 square pixels large and content does not look exceedingly long
+                    if (height > 3 && width > 3 && elem_inner.length < 1000) {
+                        if (elem.tagName === tag && elem.id === id && elem.name === name && elem_inner === inner) {
+                            matched = true;
+                            return true;
+                        }
+                        if (matched) {
+                            if (ret.length < num) {
+                                ret.push(elem_to_obj(elem));
+                            } else {
+                                return false;
+                            }
+                        }
+                    }
+                    return true;
+                });
+                return ret;
+            };
+            // Invoke the search function using parameters stored in window object
+            return find_after(laitos_pjs_tag, laitos_pjs_id, laitos_pjs_name, laitos_pjs_inner, laitos_pjs_next_n);
+        });
+        if (ret.length > 0) {
+            before_info = exact_info;
+            // Intentionally set both exact and after element information to that belonging to very last element
+            exact_info = ret[ret.length - 1];
+            after_info = ret[ret.length - 1];
+        }
+        return ret;
     };
 
 } catch (err) {
