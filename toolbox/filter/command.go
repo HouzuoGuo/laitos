2ad034a9c4c4bf359976a8380b47e7447284b242
HouzuoGuo <guohouzuo@gmail.com>
2019-12-24 12:22:01 +0000

    allow user to use TOTP derived from password PIN instead of the password PIN itself, when invoking toolbox commands over less secure channels

--- toolbox/filter/command.go
+++ toolbox/filter/command.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"strings"
 
+	"github.com/HouzuoGuo/laitos/lalog"
 	"github.com/HouzuoGuo/laitos/toolbox"
 )
 
@@ -28,30 +29,92 @@ type PINAndShortcuts struct {
 	Shortcuts map[string]string `json:"Shortcuts"`
 }
 
-var ErrPINAndShortcutNotFound = errors.New("failed to match PIN/shortcut")
+var ErrPINAndShortcutNotFound = errors.New("invalid password PIN or shortcut")
+
+/*
+getTOTP returns TOTP-based PINs that work as alternative to password PIN text input.
+TOTP based PINs are calculated based on system clock, therefore, the function returns a set of acceptable numbers in 90 seconds interval
+and user may use any of the numbers instead of password PIN, this helps to mask the real password PIN when executing toolbox commands
+over less-secure communication channels such as DNS queries and SMS text.
+
+The TOTP number set is calculated this way:
+1. List 1 = the previous, current, and upcoming TOTP 2FA codes based on password PIN string (each code is a string made of 6 digits).
+2. List 2 = the previous, current, and upcoming TOTP 2FA codes based on the password PIN string reversed.
+3. For each string from list 1, concatenate it with each string from list 2, and return the concatenation results in a set.
+*/
+func (pin *PINAndShortcuts) getTOTP() (ret map[string]bool) {
+	ret = map[string]bool{}
+	if pin.PIN == "" {
+		return
+	}
+	// Calculate TOTP using password PIN - list 1
+	prev1, current1, next1, err := toolbox.GetTwoFACodes(pin.PIN)
+	if err != nil {
+		lalog.DefaultLogger.Info("getTOTP", "", err, "failed to calculate TOTP")
+		return
+	}
+	// Reverse the password PIN
+	reversed := []rune(pin.PIN)
+	for i, j := 0, len(reversed)-1; i < j; i, j = i+1, j-1 {
+		reversed[i], reversed[j] = reversed[j], reversed[i]
+	}
+	reversedStr := string(reversed)
+	// Calculate TOTP using reversed password PIN - list 2
+	prev2, current2, next2, err := toolbox.GetTwoFACodes(reversedStr)
+	if err != nil {
+		lalog.DefaultLogger.Info("getTOTP", "", err, "failed to calculate TOTP")
+		return
+	}
+	// Concatenate codes from the first list and second list
+	for _, s1 := range []string{prev1, current1, next1} {
+		for _, s2 := range []string{prev2, current2, next2} {
+			if code := s1 + s2; len(code) != 12 {
+				lalog.DefaultLogger.Info("getTOTP", "", nil, "wrong code length - %d", len(code))
+				return
+			}
+			ret[s1+s2] = true
+		}
+	}
+	return
+}
 
 func (pin *PINAndShortcuts) Transform(cmd toolbox.Command) (toolbox.Command, error) {
 	if pin.PIN == "" && (pin.Shortcuts == nil || len(pin.Shortcuts) == 0) {
-		return toolbox.Command{}, errors.New("both PIN and shortcuts are undefined")
+		return toolbox.Command{}, errors.New("PINAndShortcut must use a password PIN, shortcut(s), or both.")
 	}
+	// Calculate password-derived TOTP codes that can be used in place of password PIN
+	totpCodes := pin.getTOTP()
+
+	// Among the input lines, look for a shortcut match, password PIN match, or TOTP code match, and leave command alone for further processing.
 	for _, line := range cmd.Lines() {
 		line = strings.TrimSpace(line)
-		// Try to match shortcut, then return expanded shortcut alone.
+		// Look for shortcut match
 		if pin.Shortcuts != nil {
 			if shortcut, exists := pin.Shortcuts[line]; exists {
 				ret := cmd
+				// Toolbox command comes from the shortcut's configuration
 				ret.Content = shortcut
 				return ret, nil
 			}
 		}
-		// Try to match PIN prefix, then remove it from successfully matched line.
-		if len(line) > len(pin.PIN) && subtle.ConstantTimeCompare([]byte(line[0:len(pin.PIN)]), []byte(pin.PIN)) == 1 {
-			ret := cmd
-			ret.Content = line[len(pin.PIN):]
-			return ret, nil
+		// Look for a password PIN match
+		if pin.PIN != "" {
+			if len(line) > len(pin.PIN) && subtle.ConstantTimeCompare([]byte(line[:len(pin.PIN)]), []byte(pin.PIN)) == 1 {
+				ret := cmd
+				// Remove matched password from the input, leave the toolbox command in-place.
+				ret.Content = line[len(pin.PIN):]
+				return ret, nil
+			}
+			// Look for a TOTP code match. The code is made of two TOTP numbers with six digits each.
+			if len(line) > 12 && totpCodes[line[:12]] {
+				ret := cmd
+				// Remove matched code from the input, leave the toolbox command in-place.
+				ret.Content = line[12:]
+				return ret, nil
+			}
 		}
 	}
-	// Nothing matched
+	// Cannot match a shortcut, password, or TOTP code, the command must not be processed further.
 	return cmd, ErrPINAndShortcutNotFound
 }
 
--- toolbox/filter/command_test.go
+++ toolbox/filter/command_test.go
@@ -1,40 +1,91 @@
 package filter
 
 import (
-	"github.com/HouzuoGuo/laitos/toolbox"
+	"fmt"
 	"testing"
+
+	"github.com/HouzuoGuo/laitos/toolbox"
 )
+
+func TestGetTOTP(t *testing.T) {
+	// Empty PIN results in no TOTP codes returned
+	pin := PINAndShortcuts{}
+	if codes := pin.getTOTP(); len(codes) != 0 {
+		t.Fatal(codes)
+	}
+	// Validate code length
+	pin.PIN = "abcdefg"
+	codes := pin.getTOTP()
+	if len(codes) != 9 {
+		t.Fatal(codes)
+	}
+	for code := range codes {
+		if len(code) != 12 {
+			t.Fatal(codes)
+		}
+	}
+	// Validate code content
+	_, current1, _, err := toolbox.GetTwoFACodes("abcdefg")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, current2, _, err := toolbox.GetTwoFACodes("gfedcba")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !codes[current1+current2] {
+		t.Fatal("missing code", current1+current2, codes)
+	}
+}
 
 func TestPINAndShortcuts_Transform(t *testing.T) {
 	pin := PINAndShortcuts{}
 	if _, err := pin.Transform(toolbox.Command{Content: "abc"}); err == nil {
 		t.Fatal("should have been an error")
 	}
-
-	pin = PINAndShortcuts{PIN: "mypin"}
-	if out, err := pin.Transform(toolbox.Command{Content: "abc"}); err != ErrPINAndShortcutNotFound || out.Content != "abc" {
+	// Match shortcut
+	pin.Shortcuts = map[string]string{"abc": "123", "def": "456"}
+	if out, err := pin.Transform(toolbox.Command{Content: "this is not a matching shortcut"}); err != ErrPINAndShortcutNotFound || out.Content != "this is not a matching shortcut" {
+		t.Fatal(out, err)
+	}
+	// Should stop processing after the first match
+	if out, err := pin.Transform(toolbox.Command{Content: "\n\n abc\nrandom line\ndef"}); err != nil || out.Content != "123" {
+		t.Fatal(out, err)
+	}
+	if out, err := pin.Transform(toolbox.Command{Content: "\nrandom line\n\n def \n\n"}); err != nil || out.Content != "456" {
+		t.Fatal(out, err)
+	}
+	// Match PIN
+	pin.PIN = "mypin"
+	if out, err := pin.Transform(toolbox.Command{Content: "this is not a matching pin"}); err != ErrPINAndShortcutNotFound || out.Content != "this is not a matching pin" {
 		t.Fatal(out, err)
 	}
 	if out, err := pin.Transform(toolbox.Command{Content: "mypineapple"}); err != nil || out.Content != "eapple" {
 		t.Fatal(out, err)
 	}
-	if out, err := pin.Transform(toolbox.Command{Content: "\nline\n mypineapple \nline\n"}); err != nil || out.Content != "eapple" {
+	if out, err := pin.Transform(toolbox.Command{Content: "\nrandom line\n mypineapple \nrandom line\n"}); err != nil || out.Content != "eapple" {
 		t.Fatal(out, err)
 	}
-	pin.Shortcuts = map[string]string{"abc": "123", "def": "456"}
-	if out, err := pin.Transform(toolbox.Command{Content: "nothing_to_see"}); err != ErrPINAndShortcutNotFound || out.Content != "nothing_to_see" {
+	// Continue to match shortcut when PIN is also configured
+	if out, err := pin.Transform(toolbox.Command{Content: "\nrandom line\n\n def \n\n"}); err != nil || out.Content != "456" {
 		t.Fatal(out, err)
 	}
-	if out, err := pin.Transform(toolbox.Command{Content: "\n\n mypineapple \n\n"}); err != nil || out.Content != "eapple" {
+	// Match TOTP
+	_, current1, _, err := toolbox.GetTwoFACodes("mypin")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, current2, _, err := toolbox.GetTwoFACodes("nipym")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if out, err := pin.Transform(toolbox.Command{Content: "this is not a matching totp"}); err != ErrPINAndShortcutNotFound || out.Content != "this is not a matching totp" {
 		t.Fatal(out, err)
 	}
-	if out, err := pin.Transform(toolbox.Command{Content: "\n\n abc\nline"}); err != nil || out.Content != "123" {
+	if out, err := pin.Transform(toolbox.Command{Content: current1 + current2 + "apple"}); err != nil || out.Content != "apple" {
 		t.Fatal(out, err)
 	}
-	if out, err := pin.Transform(toolbox.Command{Content: "\nline\n\n def \n\n"}); err != nil || out.Content != "456" {
-		t.Fatal(out, err)
-	}
-	if out, err := pin.Transform(toolbox.Command{Content: "ghi"}); err != ErrPINAndShortcutNotFound || out.Content != "ghi" {
+	if out, err := pin.Transform(toolbox.Command{Content: fmt.Sprintf("\nline1\n %s%sapple\nline\n", current1, current2)}); err != nil || out.Content != "apple" {
 		t.Fatal(out, err)
 	}
 }
