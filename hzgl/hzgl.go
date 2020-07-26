59d76a8405b2ee1657df691f0f46b17760d6bf44
HouzuoGuo <guohouzuo@gmail.com>
2020-07-26 19:43:45 +0300

    hzgl

--- hzgl/hzgl.go
+++ hzgl/hzgl.go
@@ -1,41 +1,29 @@
 /*
-Hello from Howard (Houzuo) Guo!
+package hzgl
 
-Let us get in touch:
+Hello from Howard (Houzuo) Guo.
 
-guohouzuo@gmail.com
-guohouzuo@hotmail.com
-i@hz.gl
-i@howard.gg
-i@houzuo.net
-i@ard.how
+### Let us get in touch 👋
+- <guohouzuo@gmail.com>, <guohouzuo@hotmail.com>
+- <i@ard.how>, <i@houzuo.net>, <i@howard.gg>, <i@hz.gl>
 
-More links:
-Blogger     https://allstarnix.blogspot.com
-Docker Hub  https://hub.docker.com/u/hzgl
-Facebook    https://www.facebook.com/HouzuoGuo
-Github      https://github.com/HouzuoGuo
-Google+     https://plus.google.com/108713804585987778690
-Gravatar    https://en.gravatar.com/howardguo
-Instagram   https://www.instagram.com/guo_howard
-Keybase     https://keybase.io/hguo
-LinkedIn    https://www.linkedin.com/in/howard-guo-2b1b6227
-Steemit     https://steemit.com/@ghz
-Telegram Messenger https://t.me/howardg
-Twitch      https://www.twitch.tv/howardgg1
-Twitter     https://twitter.com/hzguo
+### More links 🌐
+- [Blogger](https://allstarnix.blogspot.com/) [Docker Hub](https://hub.docker.com/u/hzgl) [Facebook](https://www.facebook.com/HouzuoGuo) [Gravatar](https://en.gravatar.com/howardguo)
+- [Instagram](https://www.instagram.com/guo_howard) [Keybase](https://keybase.io/hguo) [LinkedIn](https://www.linkedin.com/in/howard-guo-2b1b6227) [Medium](https://medium.com/@hzgl)
+- [Steemit](https://steemit.com/@ghz) [Telegram Messenger](https://t.me/howardg) [Twitch](https://www.twitch.tv/howardgg1) [Twitter](https://twitter.com/hzguo)
 
-GPG key fingerprint: 4096R/50FAB38C 60BD 93B6 F0C4 FCC7 1735  08D4 4132 7BD9 50FA B38C
-IANA private enterprise number: 1.3.6.1.4.1.52535
+### Get in touch via satellite 🛰️
+- Inmarsat: +870 776 258 036
+- Garmin inReach: guohouzuo@inreach.garmin.com (inReach exclusive, this is not an Email)
 
-Thank you for your visit, have a nice day!
+### Geeky stuff 🤓
+- GPG key fingerprint: 4096R/50FAB38C 60BD 93B6 F0C4 FCC7 1735 08D4 4132 7BD9 50FA B38C
+- IANA private enterprise number: 1.3.6.1.4.1.52535
+- IANA TRIP ITAD number: 1988
 
-"HZGL"
+Thank you for your visit, have a nice day! "HZGL"
 
-https://howard.gg
-https://hz.gl
-https://houzuo.net
-https://ard.how
+<http://ard.how> <http://houzuo.net> <http://howard.gg> <http://hz.gl>
 */
 
 package hzgl
