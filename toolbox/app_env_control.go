f369f4e627869ca8efe1912950168e7a0ce6e59b
HouzuoGuo <guohouzuo@gmail.com>
2021-05-08 18:50:30 +0300

    app_env_control: tune a couple more kernel parameters

--- toolbox/app_env_control.go
+++ toolbox/app_env_control.go
@@ -108,15 +108,22 @@ func TuneLinux() string {
 	// The following settings have little influence on system resources
 	assignment := map[string]string{
 		// Optimise system security
-		"kernel.sysrq":              "0",
-		"kernel.panic":              "10",
-		"kernel.randomize_va_space": "2",
+		"fs.protected_fifos":        "1",
 		"fs.protected_hardlinks":    "1",
+		"fs.protected_regular":      "2",
 		"fs.protected_symlinks":     "1",
+		"kernel.core_uses_pid":      "1",
+		"kernel.dmesg_restrict":     "1",
+		"kernel.kptr_restrict":      "1",
+		"kernel.panic":              "10",
+		"kernel.pid_max":            "4194304",
+		"kernel.randomize_va_space": "2",
+		"kernel.sysrq":              "0",
+		"kernel.yama.ptrace_scope":  "1",
 
 		// Optimise system stability in low memory situation
-		"vm.zone_reclaim_mode": "1",
 		"vm.min_free_kbytes":   strconv.FormatInt(memSizeKB/32, 10), // reserve 1MB for every 32MB of system memory
+		"vm.zone_reclaim_mode": "1",
 
 		/*
 			In earlier versions of laitos (< 1.3) IP forwarding used to be disabled right here, however, docker
@@ -124,50 +131,56 @@ func TuneLinux() string {
 			SlimerJS depends on docker container. Hence, IP forwarding is no longer disabled.
 		*/
 		// Optimise network security
-		"net.ipv4.conf.all.mc_forwarding":       "0",
-		"net.ipv6.conf.all.mc_forwarding":       "0",
-		"net.ipv4.conf.all.accept_redirects":    "0",
-		"net.ipv6.conf.all.accept_redirects":    "0",
-		"net.ipv4.conf.all.accept_source_route": "0",
-		"net.ipv6.conf.all.accept_source_route": "0",
-		"net.ipv4.conf.all.secure_redirects":    "0",
-		"net.ipv6.conf.all.secure_redirects":    "0",
-		"net.ipv4.conf.all.send_redirects":      "0",
-		"net.ipv6.conf.all.send_redirects":      "0",
-
+		"net.ipv4.conf.all.accept_redirects":         "0",
+		"net.ipv4.conf.all.accept_source_route":      "0",
+		"net.ipv4.conf.all.log_martians":             "1",
+		"net.ipv4.conf.all.mc_forwarding":            "0",
+		"net.ipv4.conf.all.rp_filter":                "1",
+		"net.ipv4.conf.all.secure_redirects":         "0",
+		"net.ipv4.conf.all.send_redirects":           "0",
+		"net.ipv4.conf.default.accept_redirects":     "0",
+		"net.ipv4.conf.default.accept_source_route":  "0",
+		"net.ipv4.conf.default.log_martians":         "1",
 		"net.ipv4.conf.default.rp_filter":            "1",
-		"net.ipv4.ip_local_port_range":               "2048\t65535",
+		"net.ipv4.conf.default.secure_redirects":     "0",
+		"net.ipv4.conf.default.send_redirects":       "0",
 		"net.ipv4.icmp_echo_ignore_broadcasts":       "1",
 		"net.ipv4.icmp_ignore_bogus_error_responses": "1",
+		"net.ipv4.ip_local_port_range":               "2048\t65535",
 		"net.ipv4.tcp_syncookies":                    "1",
+		"net.ipv6.conf.all.accept_redirects":         "0",
+		"net.ipv6.conf.all.accept_source_route":      "0",
+		"net.ipv6.conf.all.mc_forwarding":            "0",
+		"net.ipv6.conf.all.secure_redirects":         "0",
+		"net.ipv6.conf.all.send_redirects":           "0",
 
 		// Optimise network performance
 		"net.core.default_qdisc":          "fq_codel",
+		"net.core.netdev_max_backlog":     "16384",
+		"net.core.somaxconn":              "4096",
 		"net.ipv4.tcp_congestion_control": "bbr",
 		"net.ipv4.tcp_fastopen":           "3",
+		"net.ipv4.tcp_max_syn_backlog":    "2048",
+		"net.ipv4.tcp_max_tw_buckets":     "262144",
 		"net.ipv4.tcp_sack":               "1",
 
 		// Optimise network compatibility
-		"net.ipv4.tcp_mtu_probing": "2",
-		"net.ipv4.tcp_base_mss":    "1024",
-		"net.ipv4.tcp_rfc1337":     "1",
+		"net.ipv4.conf.all.promote_secondaries":     "1",
+		"net.ipv4.conf.default.promote_secondaries": "1",
+		"net.ipv4.tcp_base_mss":                     "1024",
+		"net.ipv4.tcp_mtu_probing":                  "2",
+		"net.ipv4.tcp_rfc1337":                      "1",
+		"net.ipv6.conf.all.disable_ipv6":            "0",
+		"net.ipv6.conf.default.disable_ipv6":        "0",
 
 		// Optimise network behaviour
-		"net.ipv4.tcp_tw_recycle":       "0",
-		"net.ipv4.tcp_tw_reuse":         "1",
-		"net.ipv4.tcp_synack_retries":   "3",
-		"net.ipv4.tcp_fin_timeout":      "15",
-		"net.ipv4.tcp_keepalive_time":   "120",
+		"net.ipv4.tcp_fin_timeout":      "10",
 		"net.ipv4.tcp_keepalive_intvl":  "30",
 		"net.ipv4.tcp_keepalive_probes": "4",
-	}
-	// The following settings are influenced by system memory size
-	atLeast := map[string]int64{
-		/// Optimise network resource usage
-		"net.core.somaxconn":           memSizeKB / 1024 / 512 * 256,  // 256 per 512MB of mem
-		"net.ipv4.tcp_max_syn_backlog": memSizeKB / 1024 / 512 * 512,  // 512 per 512MB of mem
-		"net.core.netdev_max_backlog":  memSizeKB / 1024 / 512 * 1024, // 1024 per 512MB of mem
-		"net.ipv4.tcp_max_tw_buckets":  memSizeKB / 1024 / 512 * 2048, // 2048 per 512MB of mem
+		"net.ipv4.tcp_keepalive_time":   "120",
+		"net.ipv4.tcp_synack_retries":   "3",
+		"net.ipv4.tcp_tw_recycle":       "0",
+		"net.ipv4.tcp_tw_reuse":         "1",
 	}
 	// Apply the optimal and return human-readable tuning result
 	var ret bytes.Buffer
@@ -175,16 +188,6 @@ func TuneLinux() string {
 		old, err := platform.SetSysctl(key, val)
 		if err == nil {
 			if old != val {
-				ret.WriteString(fmt.Sprintf("%s: %v -> %v\n", key, old, val))
-			}
-		} else {
-			ret.WriteString(fmt.Sprintf("Failed to set %s - %v\n", key, err))
-		}
-	}
-	for key, val := range atLeast {
-		old, err := platform.IncreaseSysctlInt(key, val)
-		if err == nil {
-			if old < val {
 				ret.WriteString(fmt.Sprintf("%s: %v -> %v\n", key, old, val))
 			}
 		} else {
