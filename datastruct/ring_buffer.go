530475cd61efc864d5b557529d2c0e89a194e982
HouzuoGuo <guohouzuo@gmail.com>
2021-09-12 18:34:27 +0100

    fix unlikely data race in RingBuffer

--- datastruct/ring_buffer.go
+++ datastruct/ring_buffer.go
@@ -1,7 +1,7 @@
 package datastruct
 
 import (
-	"sync/atomic"
+	"sync"
 )
 
 // RingBuffer is a rudimentary implementation of a fixed-size circular buffer.
@@ -10,6 +10,7 @@ type RingBuffer struct {
 	size    int64
 	counter int64
 	buf     []string
+	mutex   *sync.Mutex
 }
 
 // NewRingBuffer returns an initialised ring buffer.
@@ -18,15 +19,18 @@ func NewRingBuffer(size int64) *RingBuffer {
 		panic("NewRingBuffer: size must be greater than 0")
 	}
 	return &RingBuffer{
-		size: size,
-		buf:  make([]string, size),
+		size:  size,
+		buf:   make([]string, size),
+		mutex: new(sync.Mutex),
 	}
 }
 
 // Push places a new element into ring buffer.
 func (r *RingBuffer) Push(elem string) {
-	elemIndex := atomic.AddInt64(&r.counter, 1)
-	r.buf[elemIndex%r.size] = elem
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	r.counter++
+	r.buf[r.counter%r.size] = elem
 }
 
 /*
@@ -43,6 +47,8 @@ The iterator function is fed an element value as sole parameter. If the function
 immediately. The total number of elements iterated is not predictable, and iteration loop always skips empty elements.
 */
 func (r *RingBuffer) IterateReverse(fun func(string) bool) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
 	currentIndex := r.counter % r.size
 	for i := currentIndex; i >= 0; i-- {
 		value := r.buf[i]
@@ -64,6 +70,8 @@ func (r *RingBuffer) IterateReverse(fun func(string) bool) {
 
 // GetAll returns all elements (oldest to the latest) of the ring buffer in a string array.
 func (r *RingBuffer) GetAll() (ret []string) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
 	reversed := make([]string, 0, r.size)
 	r.IterateReverse(func(elem string) bool {
 		reversed = append(reversed, elem)
