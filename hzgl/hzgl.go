39e0f947b2f24e950eec91570165a5e9cd9cc066
HouzuoGuo <guohouzuo@gmail.com>
2022-04-24 17:23:22 +0100

    seo: hzgl keyword

--- hzgl/hzgl.go
+++ hzgl/hzgl.go
@@ -5,25 +5,22 @@ Hello from Howard (Houzuo) Guo.
 
 ### Let us get in touch 👋
 - <guohouzuo@gmail.com>, <guohouzuo@hotmail.com>
-- <i@ard.how>, <i@houzuo.net>, <i@howard.gg>, <i@hz.gl>
+- <i@houzuo.net>, <i@howard.gg>, <i@hz.gl>
 
 ### More links 🌐
 - [Blogger](https://allstarnix.blogspot.com/) [Docker Hub](https://hub.docker.com/u/hzgl) [Facebook](https://www.facebook.com/HouzuoGuo) [Gravatar](https://en.gravatar.com/howardguo)
 - [Instagram](https://www.instagram.com/guo_howard) [Keybase](https://keybase.io/hguo) [LinkedIn](https://www.linkedin.com/in/howard-guo-2b1b6227) [Medium](https://medium.com/@hzgl)
 - [Steemit](https://steemit.com/@ghz) [Telegram Messenger](https://t.me/howardg) [Twitch](https://www.twitch.tv/howardgg1) [Twitter](https://twitter.com/hzguo)
 
-### Get in touch via satellite 🛰️
+### Get in touch via satellite📡
 - Inmarsat: +870 776 258 036
-- Garmin inReach: guohouzuo@inreach.garmin.com (inReach exclusive, this is not an Email)
 
 ### Geeky stuff 🤓
 - GPG key fingerprint: 4096R/50FAB38C 60BD 93B6 F0C4 FCC7 1735 08D4 4132 7BD9 50FA B38C
 - IANA private enterprise number: 1.3.6.1.4.1.52535
 - IANA TRIP ITAD number: 1988
 
-Thank you for your visit, have a nice day! "HZGL"
-
-<http://ard.how> <http://houzuo.net> <http://howard.gg> <http://hz.gl>
+Thank you for your visit, have a nice day!
 */
 
 package hzgl
