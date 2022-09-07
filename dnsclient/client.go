6c93465749d3b6dbc8c2dc9ad2d18805dc8d8813
HouzuoGuo <guohouzuo@gmail.com>
2022-09-07 18:21:07 +0100

    tcpoverdns: use new increased estimate for the default segment length

--- dnsclient/client.go
+++ dnsclient/client.go
@@ -404,8 +404,8 @@ func (client *Client) Stop() {
 // name.
 func OptimalSegLen(dnsHostName string) int {
 	// The maximum DNS host name is 253 characters.
-	// At present the encoding efficiency is ~55% at the worst case scenario.
-	approxLen := float64(250-len(dnsHostName)) * 0.55
+	// At present the encoding efficiency is ~62% at the worst case scenario.
+	approxLen := float64(250-len(dnsHostName)) * 0.62
 	ret := int(approxLen)
 	if ret < 0 {
 		return 0
--- dnsclient/client_test.go
+++ dnsclient/client_test.go
@@ -162,7 +162,7 @@ func TestClient_HTTP(t *testing.T) {
 
 func TestOptimalSegLen(t *testing.T) {
 	got := OptimalSegLen("example.com")
-	want := 131
+	want := 148
 	if got != want {
 		t.Fatalf("got: %v, want: %v", got, want)
 	}
