f30adbc0969c27ebf0acd858f5d91ca2e175e819
HouzuoGuo <guohouzuo@gmail.com>
2025-09-11 19:36:33 +0100

    extra: use new win11 convention to call register-scheduledtask

--- extra/windows/setup.ps1
+++ extra/windows/setup.ps1
@@ -9,10 +9,9 @@ $laitosWD = Read-Host -Prompt 'Which directory does laitos program data (JSON co
 $laitosAction = New-ScheduledTaskAction -Execute $laitosCmd -Argument $laitosArg -WorkingDirectory $laitosWD
 $laitosTrigger = New-ScheduledTaskTrigger -AtStartup
 $laitosSettings = New-ScheduledTaskSettingsSet -MultipleInstances IgnoreNew -RunOnlyIfNetworkAvailable -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopOnIdleEnd -RestartInterval (New-TimeSpan -Minutes 1) -RestartCount 100 -ExecutionTimeLimit (New-TimeSpan -Days 3650)
-$laitosTask = New-ScheduledTask -Action $laitosAction -Trigger $laitosTrigger -Settings $laitosSettings
 $laitosUser = Read-Host -Prompt 'What administrator user will laitos run as? E.g. Administrator'
 $laitosPassword = Read-Host -AsSecureString -Prompt 'What is the administrator password?'
 $laitosCred = New-Object System.Management.Automation.PSCredential -ArgumentList $laitosUser, $laitosPassword
-$laitosTask | Register-ScheduledTask -Force -TaskName laitos -User $laitosUser -Password $laitosCred.GetNetworkCredential().Password
+Register-ScheduledTask -Action $laitosAction -Trigger $laitosTrigger -Settings $laitosSettings -Force -TaskName laitos -User $laitosUser -Password $laitosCred.GetNetworkCredential().Password -RunLevel Highest
 
 Read-Host -Prompt 'laitos is now ready to start automatically upon system boot, enter anything to terminate the setup script.'
